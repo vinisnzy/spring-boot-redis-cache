61b3ec187e659c584e2d23a68b4cf5409716cb58
Vinícius <manoseilacara123@gmail.com>
2025-10-20 20:47:14 -0300

    feat: add caching with redis

--- spring-boot-redis-cache-initial/src/main/java/com/techie/springbootrediscache/service/ProductService.java
+++ spring-boot-redis-cache-initial/src/main/java/com/techie/springbootrediscache/service/ProductService.java
@@ -3,17 +3,22 @@ package com.techie.springbootrediscache.service;
 import com.techie.springbootrediscache.dto.ProductDto;
 import com.techie.springbootrediscache.entity.Product;
 import com.techie.springbootrediscache.repository.ProductRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
 
+    public static final String PRODUCT_CACHE = "products";
     private final ProductRepository productRepository;
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
+    @CachePut(value = PRODUCT_CACHE, key = "#result.id()")
     public ProductDto createProduct(ProductDto productDto) {
         var product = new Product();
         product.setName(productDto.name());
@@ -24,6 +29,7 @@ public class ProductService {
                 savedProduct.getPrice());
     }
 
+    @Cacheable(value = PRODUCT_CACHE, key = "#productId")
     public ProductDto getProduct(Long productId) {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new IllegalArgumentException("Cannot find product with id " + productId));
@@ -31,6 +37,7 @@ public class ProductService {
                 product.getPrice());
     }
 
+    @CachePut(value = PRODUCT_CACHE, key = "#result.id()")
     public ProductDto updateProduct(ProductDto productDto) {
         Long productId = productDto.id();
         Product product = productRepository.findById(productId)
@@ -44,6 +51,7 @@ public class ProductService {
                 updatedProduct.getPrice());
     }
 
+    @CacheEvict(value = PRODUCT_CACHE, key = "#productId")
     public void deleteProduct(Long productId) {
         productRepository.deleteById(productId);
     }
